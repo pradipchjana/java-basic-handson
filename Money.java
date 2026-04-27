c7d2b431ab51277458dcf1bc7c0eb935ccfca606
Pradip Jana <janaprdip501@gmail.com>
2026-04-27 15:11:20 +0530

    done

--- Money.java
+++ Money.java
@@ -1,1 +1,17 @@
-// Money.java
+class Money {
+  int value;
+  
+  Money(int value){
+    this.value = value;
+  }
+
+  Money add( Money m){
+    int addResult = this.value + m.value;
+    return new Money(addResult);
+  }
+
+    Money minus( Money m){
+    int addResult = this.value - m.value;
+    return new Money(addResult);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,41 @@
-// StringUtils
+class StringUtils {
+  public static void main(String[] args){
+    System.out.println("do nothing");
+  }
+
+  static boolean containsChar(String str, char c) {
+    
+    for(int i = 0; i<str.length();i++){
+      if(str.charAt(i)==c) return true;
+    }
+
+    return false;
+  }
+
+  static String reverse(String str) {
+    String reverse = "";
+
+    for(int i = str.length()-1; i>=0;i--){
+      reverse += str.charAt(i);
+    }
+
+    return reverse;
+  }
+
+  static String replaceAll(String str, char a, char b) {
+    String newString = "";
+
+    for(int i = 0; i<str.length();i++){
+
+      if(str.charAt(i)==a){
+        newString += b;
+
+      }else{
+      newString += str.charAt(i);
+
+      }
+    }
+
+    return newString;
+  }
+}
