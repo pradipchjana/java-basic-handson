4aa8e4fb7bcd5b5399cd90c72a200ed343580b80
ashishku <ashishkujoy@gmail.com>
2026-04-27 14:45:24 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main() {
+    System.out.println(StringUtils.containsChar("Hello", 'l')); // true
+    System.out.println(StringUtils.reverse("Hello")); // "olleH"
+    System.out.println(StringUtils.replaceAll("Hello"), 'l', 'L'); // "HeLLo"
+
+    Money twelveRupee = new Money(12);
+    Money tenRupee = new Money(10);
+    
+    Money twentyTwoRupee = twelveRupee.add(tenRupee);
+    Money twoRupee = twelveRupee.minus(tenRupee);
+
+    System.out.println(twentyTwoRupee.value); // 22
+    System.out.println(twoRupee.value); // 2
+
+    // twoRupee.value = 22; // error
+  }
+}
